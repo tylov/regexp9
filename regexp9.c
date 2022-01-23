c1b95defbc7ac9000d8e361e5a0a8a342379534e
Tyge Løvset <tylo@norceresearch.no>
2022-01-23 12:29:05 +0100

    Moved private definitions from header to c file. Removed test1.c

--- regexp9.c
+++ regexp9.c
@@ -37,6 +37,133 @@
 #include <unistd.h>
 #include "regexp9.h"
 
+/*********
+ * utf.h *
+ *********/
+
+typedef struct Reclass  Reclass;
+typedef struct Reinst   Reinst;
+
+enum
+{
+    UTFmax      = 3,        /* maximum bytes per rune */
+    Runesync    = 0x80,     /* cannot represent part of a UTF sequence (<) */
+    Runeself    = 0x80,     /* rune and UTF sequences are the same (<) */
+    Runeerror   = 0xFFFD        /* decoding error in UTF */
+};
+
+/*************
+ * regexp9.h *
+ *************/
+
+/*
+ *    character class, each pair of rune's defines a range
+ */
+struct Reclass{
+    Rune    *end;
+    Rune    spans[64];
+};
+
+/*
+ *    Machine instructions
+ */
+struct Reinst{
+    int    type;
+    union    {
+        Reclass    *cp;        /* class pointer */
+        Rune    r;        /* character */
+        int    subid;        /* sub-expression id for RBRA and LBRA */
+        Reinst    *right;        /* right child of OR */
+    }u1;
+    union {    /* regexp relies on these two being in the same union */
+        Reinst *left;        /* left child of OR */
+        Reinst *next;        /* next instruction for CAT & LBRA */
+    }u2;
+};
+
+/*
+ *    Reprogram definition
+ */
+struct Reprog{
+    Reinst    *startinst;    /* start pc */
+    Reclass    class[16];    /* .data */
+    Reinst    firstinst[5];    /* .text */
+};
+
+/*************
+ * regcomp.h *
+ *************/
+
+/*
+ *  substitution list
+ */
+#define uchar __reuchar
+typedef unsigned char uchar;
+#define nelem(x) (sizeof(x)/sizeof((x)[0]))
+
+#define NSUBEXP 32
+typedef struct Resublist    Resublist;
+struct    Resublist
+{
+    Resub    m[NSUBEXP];
+};
+
+/* max character classes per program */
+extern Reprog    RePrOg;
+#define    NCLASS    (sizeof(RePrOg.class)/sizeof(Reclass))
+
+/* max rune ranges per character class */
+#define NCCRUNE    (sizeof(Reclass)/sizeof(Rune))
+
+/*
+ * Actions and Tokens (Reinst types)
+ *
+ *    02xx are operators, value == precedence
+ *    03xx are tokens, i.e. operands for operators
+ */
+#define RUNE        0177
+#define OPERATOR    0200    /* Bitmask of all operators */
+#define START       0200    /* Start, used for marker on stack */
+#define RBRA        0201    /* Right bracket, ) */
+#define LBRA        0202    /* Left bracket, ( */
+#define OR          0203    /* Alternation, | */
+#define CAT         0204    /* Concatentation, implicit operator */
+#define STAR        0205    /* Closure, * */
+#define PLUS        0206    /* a+ == aa* */
+#define QUEST       0207    /* a? == a|nothing, i.e. 0 or 1 a's */
+#define ANY         0300    /* Any character except newline, . */
+#define ANYNL       0301    /* Any character including newline, . */
+#define NOP         0302    /* No operation, internal use only */
+#define BOL         0303    /* Beginning of line, ^ */
+#define EOL         0304    /* End of line, $ */
+#define CCLASS      0305    /* Character class, [] */
+#define NCCLASS     0306    /* Negated character class, [] */
+#define END         0377    /* Terminate: match found */
+
+/*
+ *  regexec execution lists
+ */
+#define LISTSIZE    10
+#define BIGLISTSIZE    (10*LISTSIZE)
+typedef struct Relist    Relist;
+struct Relist
+{
+    Reinst*      inst;      /* Reinstruction of the thread */
+    Resublist    se;        /* matched subexpressions in this thread */
+};
+typedef struct Reljunk    Reljunk;
+struct    Reljunk
+{
+    Relist*  relist[2];
+    Relist*  reliste[2];
+    int      starttype;
+    Rune     startchar;
+    char*    starts;
+    char*    eol;
+    Rune*    rstarts;
+    Rune*    reol;
+};
+
 #define nil 0
 #define exits(x) exit(x && *x ? 1 : 0)
 
@@ -326,6 +453,7 @@ static    void    pushand(Reinst*, Reinst*);
 static    void    pushator(int);
 static    void    evaluntil(int);
 static    int    bldcclass(void);
+extern    void    regerror9(char*);
 
 static jmp_buf regkaboom;
 
